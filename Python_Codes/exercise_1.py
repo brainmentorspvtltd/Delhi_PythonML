42bfd08deb94247f0e292074256670485cc9d6da
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-09-29 11:58:21 +0530

    Add files via upload

---
+++ Python_Codes/exercise_1.py
@@ -0,0 +1,16 @@
+products = [
+    {'p_id':1,'p_name':'apple iphone 10','p_price':67000,'p_cat':'mobile','p_brand':'apple'},
+    {'p_id':2,'p_name':'apple macbook pro','p_price':127000,'p_cat':'laptop','p_brand':'apple'},
+    {'p_id':3,'p_name':'samsung galaxy s5','p_price':70200,'p_cat':'mobile','p_brand':'samsung'},
+    {'p_id':4,'p_name':'asus zenbook','p_price':81000,'p_cat':'laptop','p_brand':'asus'},
+    {'p_id':5,'p_name':'asus mobile phone','p_price':27500,'p_cat':'mobile','p_brand':'asus'},
+    {'p_id':6,'p_name':'samsung smart tv','p_price':50500,'p_cat':'tv','p_brand':'samsung'}
+    ]
+
+to_search = input("Enter your search : ")
+
+#1. Build a search engine, where user can write product name, brand or category
+#2. Store the data in a list
+#3. Ask user, if he wants to sort the data based on price...
+#4. If user enter yes, then ask user to sorting order (asc or desc...)
+#5. Print the sorted data....
