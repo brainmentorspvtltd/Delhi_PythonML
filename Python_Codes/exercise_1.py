208a9b324b96cc88c9d8aed0e5bf1db0a91ae774
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-09-30 11:29:40 +0530

    Add files via upload

--- Python_Codes/exercise_1.py
+++ Python_Codes/exercise_1.py
@@ -8,9 +8,49 @@ products = [
     ]
 
 to_search = input("Enter your search : ")
-
+to_search = to_search.lower()
 #1. Build a search engine, where user can write product name, brand or category
 #2. Store the data in a list
 #3. Ask user, if he wants to sort the data based on price...
 #4. If user enter yes, then ask user to sorting order (asc or desc...)
 #5. Print the sorted data....
+
+data = []
+# for i in range(len(products)):
+#     if products[i]['p_name'] == to_search or products[i]['p_cat'] == to_search or products[i]['p_brand'] == to_search:
+#         data.append(products[i])
+#         print(products[i])
+for i in range(len(products)):
+    cond_1 = to_search in products[i]['p_name']
+    cond_2 = to_search in products[i]['p_cat']
+    cond_3 = to_search in products[i]['p_brand']
+    if cond_1 or cond_2 or cond_3:
+        data.append(products[i])
+        print(products[i])
+
+print("""
+1. Sort Data
+2. Quit
+""")
+user_ch = input("Do you want to sort the products ? ")
+if user_ch == "1":
+    print("""
+    1. Sort Low to High
+    2. Sort High to Low
+    """)
+    ch = input("Enter your choice : ")
+    if ch == "1":
+        sorted_data = sorted(data, key = lambda x : x['p_price'])
+        for item in sorted_data:
+            print(item)
+    elif ch == "2":
+        sorted_data = sorted(data, key = lambda x: x['p_price'], reverse=True)
+        for item in sorted_data:
+            print(item)
+    else:
+        print("Invalid Choice...")
+else:
+    pass
+
+
+
---
+++ Python_Codes/movie_recommendation_2.py
@@ -0,0 +1,11 @@
+dataset = {
+    "action" : ["matrix","batman","superman","dabang","avengers","thor","hulk","krrish","ironman"],
+    "comedy" : ["the mask","dhamaal","bala","housefull","golmaal","hera pheri","golmaal 2","dhamaal 2","hera pheri 2"],
+    "drama" : ["dabang","krrish","hera pheri","bala","kahani","batla house","kgf","zero","sultan"],
+    "thriller" : ["kahani","kgf","batla house","madras cafe","uri","raw","lucy","the ring","oculus"],
+    "horror" : ["oculus","the ring","it","the ring 2","evil dead","conjuring","conjuring 2","bhootnath","aatma"]
+}
+
+user_1 = {'batman','kgf','bala','thor','kahani','lucy','raw','uri'}
+user_2 = {'matrix','batman','kgf','thor','raw','batla house','the ring','golmaal'}
+
