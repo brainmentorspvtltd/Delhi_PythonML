0c0267decccd229f76f61f5c0b0ab51211023d65
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-10-01 11:53:22 +0530

    Add files via upload

--- Python_Codes/movie_recommendation_2.py
+++ Python_Codes/movie_recommendation_2.py
@@ -9,3 +9,23 @@ dataset = {
 user_1 = {'batman','kgf','bala','thor','kahani','lucy','raw','uri'}
 user_2 = {'matrix','batman','kgf','thor','raw','batla house','the ring','golmaal'}
 
+# 1. Find sim movies of user_1 and user_2
+# 2. Find Similarity Score of sim movies with all the genres
+# 3. Get the category name which is most common b/w both users
+# 4. Recommend movies from user_2 of that category to user_1 that he has not watched yet...
+
+simMovies = user_1.intersection(user_2)
+print(simMovies)
+
+scores = {}
+for key in dataset:
+    movies = dataset[key]
+    numer = len(simMovies.intersection((set(movies))))
+    denom = len(simMovies.union((set(movies))))
+    simScore = numer/denom
+    scores[key] = simScore
+
+print(scores)
+
+
+
