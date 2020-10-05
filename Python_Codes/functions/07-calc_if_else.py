e7ae9313feaa027c58889caf4d48f9a7cd7104c5
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-10-05 11:59:12 +0530

    Add files via upload

---
+++ Python_Codes/functions/07-calc_if_else.py
@@ -0,0 +1,36 @@
+def add(x,y):
+    z = x + y
+    print("Sum is",z)
+
+def sub(x,y):
+    z = x - y
+    print("Sub is",z)
+
+def mul(x,y):
+    z = x * y
+    print("Mul is",z)
+
+def div(x,y):
+    z = x / y
+    print("Div is",z)
+
+print("""
+1. Add
+2. Sub
+3. Mul
+4. Div
+""")
+
+ch = input("Enter your choice : ")
+
+if ch == "1":
+    pass
+elif ch == "2":
+    pass
+elif ch == "3":
+    pass
+elif ch == "4":
+    pass
+else:
+    print("Invalid Choice")
+
