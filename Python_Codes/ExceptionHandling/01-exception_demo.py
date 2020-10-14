f15c3f8267e1d8cfe3f32df55cd8e5277895d9e8
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-10-14 11:52:38 +0530

    Add files via upload

---
+++ Python_Codes/ExceptionHandling/01-exception_demo.py
@@ -0,0 +1,14 @@
+a = int(input("Enter first num : "))
+b = int(input("Enter second num : "))
+
+c = a + b
+print("Sum is",c)
+
+d = a - b
+print("Sub is",d)
+
+e = a / b
+print("Div is",e)
+
+f = a * b
+print("Mul is",f)
