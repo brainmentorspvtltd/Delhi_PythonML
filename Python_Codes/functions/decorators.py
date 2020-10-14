f606c86d5051b7169ae5ceb332a8fcec0378f728
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-10-14 11:52:34 +0530

    Add files via upload

--- Python_Codes/functions/decorators.py
+++ Python_Codes/functions/decorators.py
@@ -1,22 +1,44 @@
-def add():
-    x = 5
-    y = 5
-    z = x + y
-    print("Sum is",z)
-
 login = True
-def check_login(func):
-    if login:
-        print("Login Success")
-    else:
-        print("Login Failed")
 
-    return add
+def loginNow():
+    email = input("Enter email : ")
+    pwd = input("Enter pwd : ")
+
+def check_login(func) -> "will always return a function":
+    def loginUser():
+        if login:
+            print("Already logged in...")
+            func()
+        else:
+            print("Login First")
+            loginNow()
+            print("Login Success...")
+            func()
+    return loginUser
 
 @check_login
 def comment():
     p = input("Enter your comment : ")
     print("Your comment : ",p)
 
-comment()
-# check_login(comment)
+# comment()
+# x = check_login(comment)
+# x()
+
+@check_login
+def subscribe():
+    print("Subscribed Successfully...")
+
+@check_login
+def like():
+    print("Added to liked videos")
+
+@check_login
+def reply_comment():
+    reply = input("Enter reply : ")
+    print("Reply :",reply)
+
+# comment()
+# subscribe()
+# like()
+reply_comment()
