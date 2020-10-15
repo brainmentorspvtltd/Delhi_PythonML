6957d0ffaa9f0ee188f506d74beaa1f6599a8198
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-10-15 11:12:16 +0530

    Add files via upload

--- Python_Codes/ExceptionHandling/01-exception_demo.py
+++ Python_Codes/ExceptionHandling/01-exception_demo.py
@@ -1,14 +1,20 @@
-a = int(input("Enter first num : "))
-b = int(input("Enter second num : "))
-
-c = a + b
-print("Sum is",c)
-
-d = a - b
-print("Sub is",d)
-
-e = a / b
-print("Div is",e)
-
-f = a * b
-print("Mul is",f)
+try:
+    a = int(input("Enter first num : "))
+    b = int(input("Enter second num : "))
+    c = a + b
+    d = a - b
+    e = a / b
+    f = a * b
+except ZeroDivisionError:
+    print("Cannot Divide by Zero...")
+# except ArithmeticError as err:
+#     print(err)
+except ValueError:
+    print("Invalid Input...Please enter a digit")
+except BaseException as err:
+    print(err)
+else:
+    print("Sum is", c)
+    print("Sub is", d)
+    print("Div is", e)
+    print("Mul is", f)
